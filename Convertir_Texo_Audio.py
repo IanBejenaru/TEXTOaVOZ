7f955e6d4d47de01ae7280d04e03f3f0ea611ff8
Ian Bejenaru <ianbeje17@gmail.com>
2024-11-14 07:45:56 +0100

    Probando el commit desde la terminal

--- Convertir_Texo_Audio.py
+++ Convertir_Texo_Audio.py
@@ -1,2 +1,6 @@
 # En este Script le pasaremos un texto y el se encargará de leerlo.
 
+# Paso 1: cargar las librerías necesarias.
+
+# Paso 2: guardar en las variables de texto lo que necesitamos que se diga.
+
