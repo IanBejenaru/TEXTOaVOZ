99e41c2c58b11588be57f9a75bb573dc71b01fad
IanBejenaru <ianbeje17@gmail.com>
2024-11-29 20:30:54 +0100

    creamos un tryExcept para evitar errores y mejoramos los comentarios

--- Convertir_Texo_Audio.py
+++ Convertir_Texo_Audio.py
@@ -1,13 +1,17 @@
 # Creador: Ian Bejenaru
 # Fecha: 24/11/2024
-# En este Script le pasaremos un texto y el se encargará de leerlo.
+# En este Script pasaremos un texto y el se encargará de leerlo. 
+# Tenemos un menú en el que podremos elegir las opciones de lectura de ese texto.
 #------------------------------------------------------------------
 
-# Paso 1: cargar las librerías necesarias.
+# Cargar las librerías necesarias.
 import pyttsx3
-
 # Creamos la función que va a inicializar el audio:
 def textoVoz(texto):
+        # INPUT
+        # texto: es el texto que se dirá por audio.
+        # OUTPUT: El sonido del texto.
+
         # Inicializamos el convertidor de texto a voz:
         engine = pyttsx3.init()
         # Le decimos lo que queremos que convierta a voz:
@@ -23,26 +27,23 @@ def leerLineasTXT(archivo):
         # OUTPUT: El texto línea por línea.
 
         # abrimos el archivo:
-        book = open(archivo)
-        # Leemos el archivo entero y lo guardamos en una variable:
-        book_text= book.readlines()
-        # Creamos un bucle for que recorrerá línea por línea lo que tenemos
-        for line in book_text:
-            # Llamamos a la función que nos va a permitir leer el texto
-            textoVoz(line)
-  
+        try: # Manejamos el error en caso de que cambie de nombre o de lugar el TXT.
+            book = open(archivo)
+            # Leemos el archivo entero y lo guardamos en una variable:
+            book_text= book.readlines()
+            # Creamos un bucle for que recorrerá línea por línea lo que tenemos
+            for line in book_text:
+                # Llamamos a la función que nos va a permitir leer el texto
+                textoVoz(line)
+        except FileNotFoundError:
+            print("El archivo no se encuentra")
 
-# Creamos la función que nos va a permitir leer el texto de una página web:
-def leerPaginaWeb(archivo):
-    # abrimos el archivo:
-    book = open(archivo)
 
-# Creamos un menú en el que el usuario pueda decidir si quiere escribir un texto para que se lea, que pille el texto de un archivo .txt 
-# o que lo lea de una pagina web
+# Creamos un menú en el que el usuario pueda decidir si quiere escribir un texto para que se lea o que pille el texto de un archivo .txt 
 # variable que nos permite salir del bulce:
 salir = True
 while salir:
-    opcion=input("Elige una opción que quieres convertir a texto:\n1) Escribir texto y convertir a voz\n2) Leer texto de un archivo txt\n3) Leer archivo de una WEB\n4) Salir\n")
+    opcion=input("Elige una opción que quieres convertir a texto:\n1) Escribir texto y convertir a voz\n2) Leer texto de un archivo txt\n3) Salir\n")
 
     # El usuario escribe por pantalla el texto que quiere leer:
     if opcion =="1":
@@ -54,15 +55,9 @@ while salir:
         # Pedimos al usuario que elija un archivo txt que quiere leer de su PC:
         leerLineasTXT("archivoPrueba.txt")
     elif opcion == "3":
-        # Pedimos al usuario que pegue el link de la pagina web que quiere leer:
-        pagina =1
-    # Opción salir del programa:
-    elif opcion == "4":
         # Llamamos a la función que nos va a permitir leer el texto
         textoVoz("Saliendo del programa....")
         salir = False
     # Cuando se selecciona una opcion que no existe:
     else:
         print("La opcion seleccionada no existe, prueba otra vez")
-
-
